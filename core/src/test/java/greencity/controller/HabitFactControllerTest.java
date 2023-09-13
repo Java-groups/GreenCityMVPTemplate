f7529d6a1b903ccce66c1a1ca67dd65a7953e7af
YuliaPylypenko <juljulip20@gmail.com>
2023-09-13 14:02:48 +0200

    Add test for PUT/POSTrequests

--- core/src/test/java/greencity/controller/HabitFactControllerTest.java
+++ core/src/test/java/greencity/controller/HabitFactControllerTest.java
@@ -2,10 +2,14 @@ package greencity.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import greencity.converters.UserArgumentResolver;
+import greencity.dto.habitfact.HabitFactPostDto;
+import greencity.dto.language.LanguageDTO;
 import greencity.exception.exceptions.NotFoundException;
 import greencity.exception.handler.CustomExceptionHandler;
 import greencity.service.HabitFactService;
+import greencity.service.LanguageService;
 import greencity.service.UserService;
+import greencity.validator.LanguageTranslationValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,23 +17,27 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+import java.util.List;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class HabitFactControllerTest {
 
     private static final String factsLink = "/facts";
@@ -44,6 +52,12 @@ public class HabitFactControllerTest {
     private ModelMapper modelMapper;
     @Mock
     private ObjectMapper objectMapper;
+
+    @InjectMocks
+    private LanguageTranslationValidator languageTranslationValidator;
+
+    @Mock
+    private LanguageService languageService;
     private final ErrorAttributes errorAttributes = new DefaultErrorAttributes();
 
     @BeforeEach
@@ -54,6 +68,7 @@ public class HabitFactControllerTest {
                         new UserArgumentResolver(userService, modelMapper))
                 .setControllerAdvice(new CustomExceptionHandler(errorAttributes, objectMapper))
                 .build();
+
     }
 
     @Test
@@ -94,45 +109,67 @@ public class HabitFactControllerTest {
 
     @Test
     void saveHabitFactTest() throws Exception {
-        String json = "{" +
-                "\"habit\": {" +
-                "  \"id\": 1" +
-                "}," +
-                "\"translations\": [" +
-                "  {" +
-                "    \"content\": \"content content content\"," +
-                "    \"language\": {" +
-                "      \"code\": \"en\"," +
-                "      \"id\": 2" +
-                "    }" +
-                "  }" +
-                "]" +
+        String content = "{\n" +
+                "  \"habit\": {\n" +
+                "    \"complexity\": 0,\n" +
+                "    \"id\": 0,\n" +
+                "    \"image\": \"string\"\n" +
+                "  },\n" +
+                "  \"id\": 0,\n" +
+                "  \"translations\": [\n" +
+                "    {\n" +
+                "      \"content\": \"string\",\n" +
+                "      \"factOfDayStatus\": \"POTENTIAL\",\n" +
+                "      \"id\": 0,\n" +
+                "      \"language\": {\n" +
+                "        \"code\": \"string\",\n" +
+                "        \"id\": 0\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
                 "}";
 
-        MockMultipartFile jsonFile = new MockMultipartFile("habitFactPostDto",
-                "",
-                "application/json",
-                json.getBytes());
+        LanguageDTO uaLanguage = new LanguageDTO(1L, "ua");
+        LanguageDTO enLanguage = new LanguageDTO(2L, "en");
+        LanguageDTO ruLanguage = new LanguageDTO(3L, "ru");
+        List<LanguageDTO> languages = Arrays.asList(uaLanguage, enLanguage, ruLanguage);
+        Mockito.when(languageService.getAllLanguages()).thenReturn(languages);
+        languageTranslationValidator.initialize(null);
 
-        this.mockMvc.perform(multipart(factsLink)
-                        .file(jsonFile)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(post(factsLink)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
                 .andExpect(status().isCreated());
+
+        HabitFactPostDto fact = objectMapper.readValue(content, HabitFactPostDto.class);
+
+        verify(habitFactService).save(fact);
     }
 
 
-//    @Test
-//    void updateHabitFactTest() throws Exception {
-//        HabitFactUpdateDto updateDto = new HabitFactUpdateDto();
-//
-//        mockMvc.perform(put(factsLink + "/{id}", 1L)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(updateDto)))
-//                .andExpect(status().isOk());
-//
-//        verify(habitFactService).update(updateDto, 1L);
-//    }
+    @Test
+    void updateHabitFactTest() throws Exception {
+        String content = "{\n" +
+                "  \"habit\": {\n" +
+                "    \"id\": 1\n" +
+                "  },\n" +
+                "  \"translations\": [\n" +
+                "    {\n" +
+                "      \"content\": \"Test Test Test\",\n" +
+                "      \"factOfDayStatus\": \"POTENTIAL\",\n" +
+                "      \"language\": {\n" +
+                "        \"code\": \"en\",\n" +
+                "        \"id\": 2\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
+        mockMvc.perform(put(factsLink + "/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk());
+    }
 
     @Test
     void deleteFailedHabitFactTest() throws Exception {
